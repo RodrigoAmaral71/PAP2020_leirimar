53ca57daa50346bca1952ca4a003bf41cff09ee6
RodrigoAmaral71 <62426457+RodrigoAmaral71@users.noreply.github.com>
2020-05-20 16:35:01 +0100

    Add files via upload

---
+++ PAP2.0/paper-dashboard-master/examples/confirmaAdicionarEscola.php
@@ -0,0 +1,19 @@
+<?php
+    if(isset($_POST['submit'])){
+       $nome= $_POST['escolaNome'];
+       $morada= $_POST['escolaMorada'];
+       $codigoPostal1= $_POST['escolaCodigoPostal1'];
+       $codigoPostal2= $_POST['escolaCodigoPostal2'];
+       $localidade= $_POST['escolaLocalidade'];
+       $email= $_POST['escolaEmail'];
+       $telefone= $_POST['escolaTelefone'];
+       $codigo= $_POST['escolaCodigo'];
+    }
+
+    $con=mysqli_connect('localhost', 'root','','pap2020formacao');
+    $sql="INSERT INTO escolas VALUES (0, $nome,$codigo,$morada,$codigoPostal1,$codigoPostal2,$localidade,$email,$telefone)";
+    $result=mysqli_query($con,$sql);
+
+
+header('location: escolas.php');
+?>
