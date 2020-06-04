35280aace66e31df8c8fbd14f20fc76ce297285d
RodrigoAmaral71 <rodrigoamaralc71@gmail.com>
2020-06-04 15:10:23 +0100

    Rodrigo - 4/6 - 15:09

---
+++ PAP2.0/paper-dashboard-master/examples/include/config.inc.php
@@ -0,0 +1,10 @@
+<?php
+    define("HOST","localhost");
+    define("USER","root");
+    define("PWD","");
+    define("DBASE","pap2020formacao");
+
+    define("VOID",0);
+    define("ADMIN_EDIT",1);
+
+?>
